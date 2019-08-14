ca71326637ae09c48acb0c40d874ba80e77c935a
mromig09 <mromig09@users.noreply.github.com>
2019-08-14 11:27:09 +1000

    first done

--- UnitTesting_Exercise/ClassLibrary1/Class1.cs
+++ UnitTesting_Exercise/ClassLibrary1/Class1.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using CIV_UnitTesting_Exercises;
 
 namespace ClassLibrary1
 {
-    public abstract class RecursiveCalculations
+    [TestFixture]
+    public class RecursiveCalculations
     {
-        
+        //Arrange
+
+        RecursiveCalculations Tests;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            Tests = new RecursiveCalculations();
+        }
+
+        [Test]
+        [TestCase(0, 1, 0, 1)]
+        [TestCase(0, 1, 1, 2)]
+        [TestCase(0, 1, 2, 3)]
+        [TestCase(0, 1, 3, 5)]
+        [TestCase(0, 1, 4, 8)]
+        public void FibonacciRec(int num1, int num2, int numFibs, int expected)
+        {
+            //Act
+            var Result = Tests.FibonacciRec(num1, num2, numFibs, expected);
+
+            //Assert
+            Assert.AreEqual(expected, Result);
+        }
     }
 }
