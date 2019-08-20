bbb03f6562abb2e6f33b4930f3be6ddad81d4ba4
mromig09 <mromig09@users.noreply.github.com>
2019-08-21 08:45:57 +1000

    Update Class1.cs

--- UnitTesting_Exercise/ClassLibrary1/Class1.cs
+++ UnitTesting_Exercise/ClassLibrary1/Class1.cs
@@ -38,11 +38,11 @@ namespace ClassLibrary1
         }
 
         [Test]
-        [TestCase(0, 1, 0, 1)]
-        [TestCase(0, 1, 1, 2)]
-        [TestCase(0, 1, 2, 3)]
-        [TestCase(0, 1, 3, 5)]
-        [TestCase(0, 1, 4, 8)]
+        [TestCase()]
+        [TestCase()]
+        [TestCase()]
+        [TestCase()]
+        [TestCase()]
         public void FactorialRec(int input, int ans, int expected)
         {
             //Act
