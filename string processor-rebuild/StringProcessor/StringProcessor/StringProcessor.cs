29fb6ae43f787898aef71973e8abec208d1a92a9
mromig09 <mromig09@users.noreply.github.com>
2019-08-21 11:27:58 +1000

    task complete

---
+++ string processor-rebuild/StringProcessor/StringProcessor/StringProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProcessor
+{
+    public class Processor
+    {
+        /// <summary>
+        /// Returns number of char found in word.  Case-insensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int CountChar(string word, char c)
+        {
+            int count = 0;
+            foreach (char t in word)
+            {
+                if (char.ToLower(c) == char.ToLower(t))
+                {
+                    count++;
+                }              
+            }
+            return count;
+        }
+            
+        
+        /// <summary>
+        /// Returns word in reverse.  Case sensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public string Reverse(string word)
+        {
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        /// <summary>
+        /// Replaces all spaces in a word with char.  Case sensitive.  Replaces pre and trailing spaces
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public string ReplaceSpace(string word, char c)
+        {
+           return word.Replace(' ', c);
+        }
+    }
+}
---
+++ string processor-rebuild/StringProcessor/StringProcessorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringProcessorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringProcessorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fd5ded2-1e9a-47b3-a055-eed4c94f5a7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
